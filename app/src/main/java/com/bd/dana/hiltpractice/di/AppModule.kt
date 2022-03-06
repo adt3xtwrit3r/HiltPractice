d2b5e259c32e3ce35bd9d85a27e6d9b9f2d1e29a
Mubin <mubin.ajkerdeal@gmail.com>
2022-03-06 12:07:53 +0600

    # code optimized

--- app/src/main/java/com/bd/dana/hiltpractice/di/AppModule.kt
+++ app/src/main/java/com/bd/dana/hiltpractice/di/AppModule.kt
@@ -1,25 +1,15 @@
 package com.bd.dana.hiltpractice.di
 
-import android.app.Application
-import com.bd.dana.hiltpractice.BuildConfig
 import com.bd.dana.hiltpractice.api.RetrofitUtils.retrofitInstance
 import com.bd.dana.hiltpractice.api.endpoint.ApiService
 import com.bd.dana.hiltpractice.api.endpoint.ApiService2
 import com.bd.dana.hiltpractice.helper.Constants
 import com.google.gson.Gson
-import com.google.gson.GsonBuilder
-import com.haroldadmin.cnradapter.NetworkResponseAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import okhttp3.Cache
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import java.io.File
-import java.util.concurrent.TimeUnit
 import javax.inject.Named
 import javax.inject.Singleton
 
